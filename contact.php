c203bac2496c94a5573dd26be26e0ca3e631a7fc
Rutuja-555 <52708848+Rutuja-555@users.noreply.github.com>
2022-09-28 12:52:10 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,25 @@
+<?php
+if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+  http_response_code(500);
+  exit();
+}
+
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email = strip_tags(htmlspecialchars($_POST['email']));
+$m_subject = strip_tags(htmlspecialchars($_POST['subject']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+
+$to = "rutuja.parab555@gmail.com"; // Change this email to your //
+$subject = "$m_subject:  $name";
+$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
+$header = "From: $email";
+$header .= "Reply-To: $email";
+
+$retval = mail($to,$subject,$body,$header);	
+
+if( $retval == true ) {
+            echo "Message sent successfully...";
+         }else {
+            echo "Message could not be sent...";
+         }
+?>
